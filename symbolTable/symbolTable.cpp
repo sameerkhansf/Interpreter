6296dd18a8366e69bf058b087c566cfc3cb279e0
gucci <martinezgus206006@gmail.com>
2025-04-17 11:13:04 -0700

    more comments

--- symbolTable/symbolTable.cpp
+++ symbolTable/symbolTable.cpp
@@ -24,14 +24,12 @@ void symbolTable::insertST(symbolTable * ST)
         _tail = ST;
     }
 }
+
+
 /***
-
-
-
 This function checks that there is a main function inside the program
 @return: returns true if a main procedure in the program, otherwise false
 */
-
 bool symbolTable::programHasMain()
 {
     stNode * iter = _head;
