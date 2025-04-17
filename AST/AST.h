49fa22147ee3cfd344672c5e405a059fc437850a
natemailloux <natemailloux@gmail.com>
2025-04-17 10:21:17 -0700

    Update AST.h
    changed string parsing to a single function

--- AST/AST.h
+++ AST/AST.h
@@ -22,7 +22,6 @@ public:
     void insert(ASTnode *& parent,  ASTnode *& _node, ASTnode *& child,  int origLineNum);
     void insertSibling(ASTnode *& parent, ASTnode *& _node, ASTnode *& child);
     void insertChild(ASTnode *& parent, ASTnode *& _node, ASTnode *& child);
-    void parseToEndl(node *& iter);
     void parseToGivenChar(const string& character, node *& iter);
     void traverseAST(ASTnode * head, ofstream& outFS);
     void output(const string& output);
@@ -32,7 +31,7 @@ public:
     static bool isOperator(node * iter);
     static bool isOpString(const string& iter);
     static bool isLeftAssociative(const string& iter);
-    int precedence(const string& op);
+    static int precedence(const string& op);
 
     ASTnode * parseVarDeclaration(node *& iter);
     ASTnode * parseSelectionStatement(node *& iter);
@@ -46,8 +45,7 @@ public:
     ASTnode * parseUserDefinedFunction(node *& iter);
     ASTnode * parseReturnStatement(node *& iter);
     ASTnode * parsePrintfStatement(node *& iter);
-    ASTnode * parseDoubleQuoteString(node *& iter);
-    ASTnode * parseSingleQuoteString(node *& iter);
+    ASTnode * parseString(node *& iter);
     ASTnode * parseIterationStatement(node *& iter);
 
 
