c4686b27e4647b3a5cd723b3694825d6d657ea8e
natemailloux <natemailloux@gmail.com>
2025-04-17 18:01:35 -0700

    Organized symbolTable.h

--- symbolTable/symbolTable.h
+++ symbolTable/symbolTable.h
@@ -4,6 +4,8 @@
 
 #ifndef ASSIGNMENT4_SYMBOLTABLE_H
 #define ASSIGNMENT4_SYMBOLTABLE_H
+#include <utility>
+
 #include "stNode.h"
 #include "../tokenize/tokenList.h"
 #include "../tokenize/tokenize.h"
@@ -12,31 +14,31 @@
 class symbolTable {
 
 public:
-    symbolTable(): _head{nullptr}, _next{nullptr}, _tail{nullptr} {}
-    symbolTable(string name): _name{name}, _head{nullptr}, _next{nullptr},
+    symbolTable(): _head{nullptr}, curNode{nullptr}, _next{nullptr}, _tail{nullptr} {}
+    explicit symbolTable(string name): _name{std::move(name)}, _head{nullptr}, curNode{nullptr}, _next{nullptr},
     _tail{nullptr} {}
     ~symbolTable();
 
     void insert( stNode *& _token);
     void insertST(symbolTable * ST);
-    stNode * head() { return _head; }
-    bool inUse(stNode * node, int& foundScope);
-    bool programHasMain();
-    bool checkVariableName(stNode * node);
     void output(const string& outputFile);
     void deleteNodes();
-    symbolTable * next() { return _next; }
+    void addParamList(symbolTable * PL);
     void next(symbolTable * ST) { _next = ST; }
-    symbolTable * tail() { return _tail; }
-    bool hasErrors() { return has_errors; }
+
+    bool inUse(stNode * node, int& foundScope);
+    bool programHasMain();
+
+    stNode * head() { return _head; }
     stNode * retrieveNode(const string& name, const int& scope);
+
+    symbolTable * next() { return _next; }
+    symbolTable * tail() { return _tail; }
 
 private:
     stNode * _head, * curNode;
     string _name;
     symbolTable * _next, * _tail;
-    bool has_errors = false;
-
 
 };
 
